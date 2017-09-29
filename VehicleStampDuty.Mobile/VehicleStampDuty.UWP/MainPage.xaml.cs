595b7e6963316b1dfd48a8dbf4a9ec2b3add61c6
Raj Bandi <raj.bandi@hotmail.com>
2017-09-29 10:22:43 +1000

    initial changes

---
+++ VehicleStampDuty.Mobile/VehicleStampDuty.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace VehicleStampDuty.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new VehicleStampDuty.App());
+        }
+    }
+}
